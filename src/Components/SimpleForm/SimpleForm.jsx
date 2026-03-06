dc3d5f113b686a27f7f9cb146a6146a317ab2d14
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 11:22:43 +0600

    react form explore

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -4,6 +4,15 @@ const SimpleForm = () => {
     return (
         <div>
             <h1>hi iam react form here</h1>
+
+
+            {/* Simple form code start here */}
+            <form>
+                <input type="text" name="name" id="name" placeholder='Your Name:'/>
+                <br /><br />
+                <input type="email" name="email" id="email" placeholder='Your email:'/><br /><br />
+                <button>Submit</button>
+            </form>
         </div>
     );
 };
