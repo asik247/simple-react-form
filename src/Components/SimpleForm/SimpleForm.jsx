4d421ab88c187aa12b807fc5ae974623e8dc8b2a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 11:35:04 +0600

    onSubmit and onClick different

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -4,20 +4,21 @@ const SimpleForm = () => {
     // sumbit form handler code start here;
     const handleSubmit = (e)=>{
         e.preventDefault()
-        const name = e.target.name.value;
-        const email = e.target.email.value;
-        console.log("form",e.target);
-        console.log(name,email);
+        console.log("click event not submit e.targer a btn paow jai not form",e.target);
+        // const name = e.target.name.value;
+        // const email = e.target.email.value;
+        // console.log("form",e.target);
+        // console.log(name,email);
     }
     return (
         <div>
             <h1>hi iam react form here</h1>
             {/* Simple form code start here */}
-            <form onSubmit={handleSubmit}>
+            <form >
                 <input type="text" name="name" id="name" placeholder='Your Name:'/>
                 <br /><br />
                 <input type="email" name="email" id="email" placeholder='Your email:'/><br /><br />
-                <button>Submit</button>
+                <button onClick={handleSubmit} type='submit'>Submit</button>
             </form>
         </div>
     );
