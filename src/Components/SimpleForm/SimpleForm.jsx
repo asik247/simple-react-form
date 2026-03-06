764ae86e5cdb2bc0f003a05cd4cc9b086c0aa9dd
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 11:28:32 +0600

    form declare

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const SimpleForm = () => {
+    // sumbit form handler code start here;
+    const handleSubmit = (e)=>{
+        e.preventDefault()
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        console.log("form",e.target);
+        console.log(name,email);
+    }
     return (
         <div>
             <h1>hi iam react form here</h1>
-
-
             {/* Simple form code start here */}
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input type="text" name="name" id="name" placeholder='Your Name:'/>
                 <br /><br />
                 <input type="email" name="email" id="email" placeholder='Your email:'/><br /><br />
