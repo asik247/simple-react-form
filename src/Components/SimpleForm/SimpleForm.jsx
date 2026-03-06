480f9991afc4c63cac1047595ee5c031399655ed
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 11:18:45 +0600

    simple-react-form first commit

---
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SimpleForm = () => {
+    return (
+        <div>
+            <h1>hi iam react form here</h1>
+        </div>
+    );
+};
+
+export default SimpleForm;
