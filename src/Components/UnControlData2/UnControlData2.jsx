52e7f7ee0a9fe736c72b5a78d68b18dc7d59f196
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-07 05:54:37 +0600

    done and explore controled way usign useSate

--- src/Components/UnControlData2/UnControlData2.jsx
+++ src/Components/UnControlData2/UnControlData2.jsx
@@ -1,6 +1,17 @@
-import React from 'react';
+import React, { useRef } from 'react';
 
 const UnControlData2 = () => {
+    // useRef code start hre;
+    const nameRef = useRef('');
+    const passwordRef = useRef('');
+    const textRef = useRef('');
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        const nameValue = nameRef.current.value;
+        const passwordValue = passwordRef.current.value;
+        const textValue = textRef.current.value;
+        console.log(nameValue,passwordValue,textValue);
+    }
     return (
         <div className="flex items-center justify-center min-h-screen ">
 
@@ -10,23 +21,23 @@ const UnControlData2 = () => {
                     Login Form (Uncontrolled2)
                 </h1>
 
-                <form className="space-y-5">
+                <form onSubmit={handleSubmit} className="space-y-5">
 
                     <input
-                      
+                        ref={nameRef}
                         type="text"
                         placeholder="Enter your Name"
                         className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
 
                     <input
-                        
+                        ref={passwordRef}
                         type="password"
                         placeholder="Enter your password"
                         className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
                     />
                     <input
-                        
+                        ref={textRef}
                         type="text"
                         placeholder="Enter Any Text"
                         className="w-full p-3 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400"
