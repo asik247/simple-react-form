134aa001496763897042e472957cdeb4fa302d70
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 12:23:08 +0600

    compleate useStae using form handle

--- src/Components/ControlComponent2/ControlComponent2.jsx
+++ src/Components/ControlComponent2/ControlComponent2.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const ControlComponent2 = () => {
+    // useSate code here;
+    const [myName,setMyName] = useState('');
+    const [myEmail,setMyEmail] = useState('');
+
+    // handle funk code here now;
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        console.log(myEmail,myName);
+    }
+
+
     return (
         <div>
             <h1>Control Component 2</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" value={myName} onChange={(e)=>setMyName(e.target.value)} />
+                <br /><br />
+
+                <input type="email" value={myEmail} onChange={(e)=>setMyEmail(e.target.value)} />
+                <br /><br />
+                <button>Submit</button>
+            </form>
         </div>
     );
 };
