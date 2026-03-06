e0eb0de840dba499f5e8e8c58e03bb5de062cf5a
Asikur Rahaman <mdasikur5893@gmail.com>
2026-03-06 11:46:19 +0600

    simple form done complate now next step

--- src/Components/SimpleForm/SimpleForm.jsx
+++ src/Components/SimpleForm/SimpleForm.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
 const SimpleForm = () => {
-    // sumbit form handler code start here;
-    const handleSubmit = (e)=>{
+
+    // form sumbit code start here;
+    const handleSubmit = (e) => {
         e.preventDefault()
-        console.log("click event not submit e.targer a btn paow jai not form",e.target);
-        // const name = e.target.name.value;
-        // const email = e.target.email.value;
-        // console.log("form",e.target);
-        // console.log(name,email);
+        console.log(e.target);
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+        console.log(email, password, name);
     }
     return (
         <div>
-            <h1>hi iam react form here</h1>
-            {/* Simple form code start here */}
-            <form >
-                <input type="text" name="name" id="name" placeholder='Your Name:'/>
-                <br /><br />
-                <input type="email" name="email" id="email" placeholder='Your email:'/><br /><br />
-                <button onClick={handleSubmit} type='submit'>Submit</button>
+            <h1>Explore React Form</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="name" id="name" placeholder='Your Name:' /><br />
+                <input type="email" name="email" id="email" placeholder='Your email:' /><br />
+                <input type="password" name="password" id="password" placeholder='Type Pin' /><br /><br />
+                <button type='submit'>Submit</button>
             </form>
         </div>
     );
